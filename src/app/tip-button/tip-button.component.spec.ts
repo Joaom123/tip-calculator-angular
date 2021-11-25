79b5a7a19f1536f1f5b1bb24a646fdc76fabcfc6
João Marcus <joaomarcus12769@gmail.com>
2021-11-25 00:32:51 -0300

    add testes do tip-button

--- src/app/tip-button/tip-button.component.spec.ts
+++ src/app/tip-button/tip-button.component.spec.ts
@@ -1,11 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TipButtonComponent } from './tip-button.component';
-import {FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
+import {ReactiveFormsModule} from "@angular/forms";
+import {DebugElement} from "@angular/core";
+import {By} from "@angular/platform-browser";
 
 describe('TipButtonComponent', () => {
   let component: TipButtonComponent;
   let fixture: ComponentFixture<TipButtonComponent>;
+  let tipButtonDebugElement: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -22,14 +25,21 @@ describe('TipButtonComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TipButtonComponent);
     component = fixture.componentInstance;
-    component.selectedTipValue = 15;
-    component.parentForm = new FormGroup({
-      tip: new FormControl(15)
-    })
+    tipButtonDebugElement = fixture.debugElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('input value should be equals tipValue', () => {
+    let inputElement: DebugElement = tipButtonDebugElement.query(By.css('input'));
+    expect(inputElement.nativeElement.id).toBe('tip0');
+  });
+
+  it('label content should be equals tipValue%', () => {
+    let labelElement: DebugElement = tipButtonDebugElement.query(By.css('label'));
+    expect(labelElement.nativeElement.textContent).toBe('0%');
   });
 });
