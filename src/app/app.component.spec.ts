9b42826a8f521f7ed74faebc5386e0b5c85217a5
João Marcus <joaomarcus12769@gmail.com>
2022-01-23 16:00:29 -0300

    pass tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,97 +1,92 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CurrencyPipe } from '@angular/common';
-import { TipButtonComponent } from './tip-button/tip-button.component';
 import { By } from '@angular/platform-browser';
+import { TipButtonComponent } from './tip-button/tip-button.component';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule, ReactiveFormsModule],
       providers: [CurrencyPipe],
       declarations: [AppComponent, TipButtonComponent]
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     const title = fixture.nativeElement.querySelector('#title');
 
     expect(title?.textContent).toEqual('S P L IT T E R');
   });
 
   it('initial value of number of people should be ""', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    const tipCalculatorForm = app.tipCalculatorForm;
-    const numberOfPeopleValue = tipCalculatorForm.get('numberPeople')?.value;
+    const numberOfPeopleValue = component.tipCalculatorForm.get('numberPeople')?.value;
 
     expect(numberOfPeopleValue).toBe('');
   });
 
   it('initial value of tip should be ""', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    const tipCalculatorForm = app.tipCalculatorForm;
-    const tipValue = tipCalculatorForm.get('tip')?.value;
+    const tipValue = component.tipCalculatorForm.get('tip')?.value;
 
     expect(tipValue).toBe('');
   });
 
   it('should calculateTipAmount', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    const tipAmount = app.calculateTipAmount();
+    const tipAmount = component.calculateTipAmount();
 
     expect(tipAmount).toEqual(0);
   });
 
   it('should calculate tip amount and total correctly when value of bill is changed', async () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    component.tipCalculatorForm.get('tip')?.patchValue(15);
+    component.tipCalculatorForm.get('numberPeople')?.patchValue(2);
+
+    const billInput = fixture.debugElement.query(By.css('#bill'));
+
+    billInput.nativeElement.value = '150';
+    billInput.nativeElement.dispatchEvent(new Event('blur'));
+
+    const tipAmount = component.calculateTipAmount();
+    const total = component.calculateTotal();
+    const billValue = component.tipCalculatorForm.get('bill')?.value;
+
     fixture.detectChanges();
     fixture.whenStable().then(() => {
-      const billInput = fixture.debugElement.query(By.css('#bill'));
-      const billInputElement = billInput.nativeElement;
-
-      billInputElement.value = '150';
-      billInputElement.dispatchEvent(new Event('input'));
-
-      const app = fixture.componentInstance;
-      const tipAmount = app.calculateTipAmount();
-      const total = app.calculateTotal();
-      const billValue = app.tipCalculatorForm.get('bill')?.value;
-
       expect(tipAmount).toBe(11.25);
-      expect(billValue).toBe('150');
+      expect(billValue).toBe('150.00');
       expect(total).toBe(86.25);
     });
   });
 
   it('should change value of tipAmount if value of bill is changed', async () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    component.tipCalculatorForm.get('tip')?.patchValue(15);
+    component.tipCalculatorForm.get('numberPeople')?.patchValue(2);
+
+    const billInput = fixture.debugElement.query(By.css('#bill'));
+    const billInputElement = billInput.nativeElement;
+
+    billInputElement.value = '150';
+    billInputElement.dispatchEvent(new Event('input'));
+
+    const billValue = component.tipCalculatorForm.get('bill')?.value;
+    const tipAmountValue = component.tipAmount;
+    const tipAmountValueUI = fixture.debugElement.query(By.css('#tipAmount'));
+
     fixture.detectChanges();
     fixture.whenStable().then(() => {
-      const billInput = fixture.debugElement.query(By.css('#bill'));
-      const billInputElement = billInput.nativeElement;
-
-      billInputElement.value = '150';
-      billInputElement.dispatchEvent(new Event('input'));
-
-      const app = fixture.componentInstance;
-      const billValue = app.tipCalculatorForm.get('bill')?.value;
-      const tipAmountValue = app.tipAmount;
-      const tipAmountValueUI = fixture.debugElement.query(By.css('#tipAmount'));
-      fixture.detectChanges();
-
       expect(tipAmountValue).toBe(11.25);
       expect(billValue).toBe('150');
       expect(tipAmountValueUI.nativeElement.textContent).toBe('$11.25');
